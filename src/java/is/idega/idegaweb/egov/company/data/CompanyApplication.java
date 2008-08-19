d3257359c60c2c7b3c21dc270e37434feaeb3e0c
Valdas Zemaitis <valdas@idega.com>
2008-08-19 06:55:14 +0000

    *** empty log message ***

--- src/java/is/idega/idegaweb/egov/company/data/CompanyApplication.java
+++ src/java/is/idega/idegaweb/egov/company/data/CompanyApplication.java
@@ -6,6 +6,6 @@ import com.idega.block.process.data.Case;
 import com.idega.data.IDOEntity;
 
 //	TODO: make it "real" entity
-public interface CompanyApplication extends IDOEntity, Case, is.idega.idegaweb.egov.fsk.data.Application, Application {
+public interface CompanyApplication extends IDOEntity, Case, is.idega.idegaweb.egov.fsk.data.Application/*TODO: is this needed?*/, Application {
 
 }
