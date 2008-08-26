77070b85d551a8309d28f3309bc7d09754c223c5
Juozas Zabuka <juozas@idega.com>
2008-08-26 10:44:12 +0000

    Implemented CompanyServicesViewer

--- src/java/is/idega/idegaweb/egov/company/presentation/employee/CompanyServicesViewer.java
+++ src/java/is/idega/idegaweb/egov/company/presentation/employee/CompanyServicesViewer.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Layer;
+import com.idega.presentation.text.Heading1;
+import com.idega.presentation.text.Heading3;
 import com.idega.user.data.Group;
 import com.idega.user.data.User;
 import com.idega.util.Age;
@@ -21,13 +23,14 @@ public class CompanyServicesViewer extends CompanyBlock {
 	protected void present(IWContext iwc) throws Exception {
 		super.present(iwc);
 
-		if (!getCompanyBusiness().isCompanyEmployee(iwc)) {
-			//	Add message that user has insufficient rights
-			return;
-		}
-		
 		Layer container = new Layer();
 		add(container);
+		container.add(new Heading1(getResourceBundle(iwc).getLocalizedString("services", "Services")));
+
+		if (!getCompanyBusiness().isCompanyEmployee(iwc)) {
+			container.add(new Heading3(getResourceBundle(iwc).getLocalizedString("insufficient_rights", "Insufficient rights")));
+			return;
+		}
 
 		User user = iwc.getCurrentUser();
 		Collection<Application> allApplications = getCompanyBusiness().getApplicationHome().findAll();
@@ -37,26 +40,26 @@ public class CompanyServicesViewer extends CompanyBlock {
 			boolean appAdded = false;
 			for (Group group : app.getGroups()) {
 				try {
-					if (!appAdded && getUserBusiness(iwc).isMemberOfGroup(Integer.valueOf(group.getId()), user)) {
+					if (!appAdded && (getUserBusiness(iwc).isMemberOfGroup(Integer.valueOf(group.getId()), user) || iwc.isSuperAdmin())) {
 						userApplicationList.add(app);
 						appAdded = true;
-					}	
-				} catch(NumberFormatException e) {
+					}
+				} catch (NumberFormatException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
+
 		if (ListUtil.isEmpty(userApplicationList)) {
-			//	TODO: add message that no services found
+			container.add(new Heading3(getResourceBundle(iwc).getLocalizedString("no_services_faund", "No services faund")));
 			return;
 		}
-		
+
 		Age[] ages = null;
 		boolean checkAges = false;
 		ages = getApplicationBusiness(iwc).getAgesForUserAndChildren(user);
 		checkAges = (ages != null);
-		
+
 		container.add(getApplicationList(iwc, checkAges, userApplicationList, ages));
 	}
 }
