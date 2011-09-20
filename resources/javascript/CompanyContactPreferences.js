c6a35d1934ea23eba2188fe8c0c18a87ce9a2ae2
Thorhallur Helgason <laddi@idega.com>
2011-09-20 13:35:04 +0000

    Adding loading messages

--- resources/javascript/CompanyContactPreferences.js
+++ resources/javascript/CompanyContactPreferences.js
@@ -4,14 +4,24 @@ jQuery(document).ready(function() {
 	jQuery('#userSSNInput').keyup(function() {
 		var ssn = jQuery(this).val();
 		if (ssn.length == 10) {
+			showLoadingMessage('Loading...');
 			CompanyApplicationBusiness.getUser(ssn, {
 				callback: function(userInfo) {
+					closeAllLoadingMessages();
+
 					if (userInfo != null) {
 						jQuery('#userNameInput').val(userInfo.userName);
 						jQuery('#userNameHidden').val(userInfo.userName);
 						jQuery('input[name="cap_email"]').val(userInfo.userEmail);
 						jQuery('input[name="cap_phn_w"]').val(userInfo.userWorkPhone);
 						jQuery('input[name="cap_phn_m"]').val(userInfo.userMobilePhone);
+					}
+					else {
+						jQuery('#userNameInput').val('');
+						jQuery('#userNameHidden').val('');
+						jQuery('input[name="cap_email"]').val('');
+						jQuery('input[name="cap_phn_w"]').val('');
+						jQuery('input[name="cap_phn_m"]').val('');
 					}
 				}
 			});
